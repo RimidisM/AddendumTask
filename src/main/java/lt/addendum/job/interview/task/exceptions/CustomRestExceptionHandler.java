0da968eda9b4fe56647bf913ccb534c15ea30a87
RimidisM <52507290+RimidisM@users.noreply.github.com>
2021-08-02 22:06:04 +0300

    utilities exceptions added

---
+++ src/main/java/lt/addendum/job/interview/task/exceptions/CustomRestExceptionHandler.java
@@ -0,0 +1,19 @@
+package lt.addendum.job.interview.task.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {RecordNotFoundException.class})
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getLocalizedMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
